275af484868abc8d0794443ad6da79593593f6db
FlickNBA <flicknba@icloud.com>
2023-06-23 02:03:06 +0200

    Homepage ready

--- src/home.js
+++ src/home.js
@@ -14,11 +14,19 @@ export function home(root) {
 
     imageDiv.classList.add('imageDiv');
 
-    const foodImages = ['chili.jpg', 'egg.jpg', 'noodles.jpg'];
+    const foodImages = ['egg.jpg', 'fish.jpg', 'noodles.jpg', 'soup.jpg'];
 
-    let chosenImage = foodImages[Math.floor(Math.random() * foodImages.length)];
+    const R = Math.floor(Math.random() * foodImages.length);
+
+    const foodTexts = ['fried and delicious egg', 'orange curry fish', 'oriental spicy noodles', 'traditional Pho soup'];
+
+    let chosenImage = foodImages[R];
+
+    let chosenText = foodTexts[R];
 
     let foodImage = document.createElement('img');
+
+    foodImage.classList.add('foodImage');
 
     foodImage.src = `./${chosenImage}`;
 
@@ -30,7 +38,15 @@ export function home(root) {
 
     let textDiv = document.createElement('div');
 
+    let pText = `Welcome to our website. We offer quality Thai food such as <strong>${chosenText}</strong> you can see on your left! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mauris tellus, ornare a maximus ac, lacinia ac purus. Morbi non ipsum eu urna gravida fringilla. Pellentesque suscipit ligula nibh, sed dignissim libero dictum quis. Suspendisse rutrum libero quam, et posuere quam bibendum vulputate. Curabitur eu luctus lectus, faucibus viverra nisi.`
+
     textDiv.classList.add('textDiv');
+
+    let P = document.createElement('p');
+
+    P.innerHTML = pText;
+
+    textDiv.appendChild(P);
 
     container.appendChild(textDiv);
 
